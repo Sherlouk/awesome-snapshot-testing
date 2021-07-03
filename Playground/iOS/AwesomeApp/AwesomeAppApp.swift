0a15d380d9f6514f7a7a1842a1666504fcc9d87a
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-07-03 14:23:53 +0100

    Add iOS Playground

---
+++ Playground/iOS/AwesomeApp/AwesomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeAppApp.swift
+//  AwesomeApp
+//
+//  Created by James Sherlock on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Playground/iOS/AwesomeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwesomeApp
+//
+//  Created by James Sherlock on 03/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
