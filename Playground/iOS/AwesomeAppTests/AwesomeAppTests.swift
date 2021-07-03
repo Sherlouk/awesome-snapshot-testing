eeec1fe7f6b32ceaf28dfd89637a080baa095a7c
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-07-03 14:42:55 +0100

    Add example test

--- Playground/iOS/AwesomeAppTests/AwesomeAppTests.swift
+++ Playground/iOS/AwesomeAppTests/AwesomeAppTests.swift
@@ -8,26 +8,48 @@
 import XCTest
 @testable import AwesomeApp
 
+import SnapshotTesting
+import SnapshotTestingStitch
+import AccessibilitySnapshot
+import ios_accessibility_text_snapshot
+
 class AwesomeAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let view = ContentView()
+        
+        assertSnapshot(
+            matching: view,
+            as: .stitch(strategies: [
+                ("Light Mode",
+                 .image(
+                    layout: .device(config: .iPhone8),
+                    traits: UITraitCollection(userInterfaceStyle: .light)
+                 )
+                ),
+                
+                ("Dark Mode",
+                 .image(
+                    layout: .device(config: .iPhone8),
+                    traits: UITraitCollection(userInterfaceStyle: .dark)
+                 )
+                ),
+                
+                ("Small Text",
+                 .image(
+                    layout: .device(config: .iPhone8),
+                    traits: UITraitCollection(preferredContentSizeCategory: .small)
+                 )
+                ),
+                
+                ("Large Text",
+                 .image(
+                    layout: .device(config: .iPhone8),
+                    traits: UITraitCollection(preferredContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+                 )
+                )
+            ]
+        ))
     }
 
 }
